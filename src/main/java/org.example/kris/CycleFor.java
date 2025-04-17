006dfaf128909b2e8a0a8877c09d1d4f598a4d63
zhelonin_aleksandr <zhelonin.2@yandex.ru>
2025-04-17 23:12:37 +0300

    Убрал лишнюю табуляцию + исправил ошибку

--- src/main/java/org.example/kris/CycleFor.java
+++ src/main/java/org.example/kris/CycleFor.java
@@ -1,14 +1,21 @@
 package org.example.kris;
 
+import java.util.Scanner;
+
 public class CycleFor {
-
     public static void main(String[] args) {
-        for(int i = 10; i>=1;i--) {
-            int count = 10 - i;
-            for (int j = 0; j < count; j++)
-                System.out.print(8);
-            System.out.println();
-        }
+        Scanner pensiAge = new Scanner (System.in);
+        int age = pensiAge.nextInt();
+        boolean isNePensia = (20 <= age) || (age <= 60);
+        boolean isPensia = (20 >= age) || (age >= 60);
+        if (isPensia ){
 
-    }
+            }
+        }
 }
+
+
+
+
+
+
